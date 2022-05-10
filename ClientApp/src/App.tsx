ef3198ecfd0dc22d548c93381fa613591f367f33
lodashreload <icarusjapan@gmail.com>
2022-05-09 22:18:41 -0400

    uninstall @type react

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import logo from '../images/logo.svg'
 
 
 export function App() {
   return <div>
     <header>
-      <img src={logo} className="logo" alt="logo" />
+      {/* <img src={logo} className="logo" alt="logo" /> */}
       <h1>BikeMi</h1>
       <div className="SignIn">Sign In</div>
     </header>

cac7b43c3a95c3acd75282de35b96e7a05aa0eef
lodashreload <icarusjapan@gmail.com>
2022-05-09 22:06:08 -0400

    having issues with svg file

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from '../images/logo.svg'
+
 
 export function App() {
   return <div>
     <header>
+      <img src={logo} className="logo" alt="logo" />
       <h1>BikeMi</h1>
+      <div className="SignIn">Sign In</div>
     </header>
     
      </div>

1252feb9084e0ec01c43321be26193b43943341d
lodashreload <icarusjapan@gmail.com>
2022-05-09 22:24:21 -0400

    still cant load local images

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import logo from '../images/logo.png'
+import { Landing } from './Pages/Landing'
 
+
+console.log(logo);
 
 export function App() {
   return <div>
     <header>
-      {/* <img src={logo} className="logo" alt="logo" /> */}
+      <img src={logo} className="logo" alt="logo" />
       <h1>BikeMi</h1>
       <div className="SignIn">Sign In</div>
     </header>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
     
      </div>
 }
